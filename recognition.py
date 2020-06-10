d1984b3cb7ed5ae740c290ba610b310520d27ffc
sid0312 <43300441+sid0312@users.noreply.github.com>
2020-06-10 21:35:47 +0530

    Added license-plate recognition

---
+++ recognition.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 10 20:39:56 2020
+
+@author: siddh
+"""
+
+import cv2
+import argparse
+import sys
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+if len(sys.argv)==2:
+	img = cv2.imread(sys.argv[1])
+	print(pytesseract.image_to_string(img))
+
+elif len(sys.argv)<2:
+	print("Enter second argument as image")
+
+else:
+	print("Too many arguments")
+	print("Filename and path to image")
